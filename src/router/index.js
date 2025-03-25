1ae0466b238d37fd4cd104f8c9a8fc8d135caef3
906051999 <906051999@qq.com>
2025-03-26 02:52:30 +0800

    refactor(router): 更新路由基础路径为/ni-win-le-ma/
    更新index.html中的favicon和脚本路径，并修改路由配置以支持新的基础路径/ni-win-le-ma/

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router' // 修改这里
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import QuizView from '../views/QuizView.vue'
 import ResultView from '../views/ResultView.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(), // 修改这里
+  history: createWebHashHistory('/ni-win-le-ma/'), // 修改这里，添加base路径
   routes,
 })
 
